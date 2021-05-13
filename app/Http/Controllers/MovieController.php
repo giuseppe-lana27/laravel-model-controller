beda8daa5979e269eda586a3c21e7131b6b5e2c9
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-05-13 16:56:59 +0200

    aggiunto controller per movies e stampa a schermo

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 use App\Movie;
 
 class MovieController extends Controller
 {
     public function index()
     {
-        $movies = Movie::all();
-        dd($movies);
+        $movies = Movie::all();        
+        return view('movies', ['movies' => $movies]);
+        
     }
 }
--- database/migrations/2021_05_13_133656_create_movies_table.php
+++ database/migrations/2021_05_13_133656_create_movies_table.php
@@ -14,6 +14,7 @@ class CreateMoviesTable extends Migration
     public function up()
     {
         Schema::create('movies', function (Blueprint $table) {
+            $table->id();
             $table->string('title', 100);
             $table->string('author', 50);
             $table->string('genre', 50);
